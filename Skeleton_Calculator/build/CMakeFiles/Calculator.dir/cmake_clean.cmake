e5bae44bc26b4788bb7c291fcb90f07d3df63554
Sushant <pi@raspberry.com>
2020-03-31 02:13:59 -0500

    cse2100 added

---
+++ Skeleton_Calculator/build/CMakeFiles/Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator.dir/src/global.cpp.o"
+  "CMakeFiles/Calculator.dir/src/main.cpp.o"
+  "Calculator.pdb"
+  "Calculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
